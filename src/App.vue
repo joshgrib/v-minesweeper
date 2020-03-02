c83aa5767dfd4121ee035f2eed5bcaa589445eb5
Josh Gribbon <josh.gribbon@sphereco.com>
2020-03-02 10:01:56 -0500

    Remove unused boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>v-minesweeper</h1>
+    <p>This should soon be a Minesweeper game implemented in Vue</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
